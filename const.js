8a370d05e636864331e522faf6ebe900236b7ce6
Andrey <avs2604@gmail.com>
2022-07-02 23:10:15 +0300

    gg

---
+++ const.js
@@ -0,0 +1,18 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+/game - Игры
+`
+const text = `
+Отличная игра, что бы расслабиться!
+<a href="https://avsss2604.github.io/ZmeykKKKa.github.io/">Нажми и играй!</a>
+
+`
+
+const text1 = `
+Любителям пощекотать нервишки)
+<a href="https://avsss2604.github.io/FlyBiird.github.io/">Нажми и играй!</a>
+`
+module.exports.commands = commands
+module.exports.text = text
+module.exports.text1 = text1
